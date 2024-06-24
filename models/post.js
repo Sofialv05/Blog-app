e7e0f1ddcd35caddf0f28ac68a34b27af90bff61
Sofialv05 <sofalvsy@gmail.com>
2024-06-24 22:46:21 +0700

    models' relation

--- models/post.js
+++ models/post.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
     /**
@@ -11,17 +9,22 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Post.belongsTo(models.Category);
+      Post.belongsTo(models.User);
     }
   }
-  Post.init({
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT,
-    imgUrl: DataTypes.STRING,
-    categoryId: DataTypes.INTEGER,
-    authorId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Post',
-  });
+  Post.init(
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.TEXT,
+      imgUrl: DataTypes.STRING,
+      categoryId: DataTypes.INTEGER,
+      authorId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Post",
+    }
+  );
   return Post;
 };
